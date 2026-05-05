5cf0bf854eb3fcf02689e38d3767f2dfa36b2ec3
Pavani Yadav <pavaniyadav093@gmail.com>
2026-05-05 14:51:40 +0000

    Update files

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 int main() {
     std::cout << "Hello from my HP Chromebook!" << std::endl;
+    std::cout << "My name is Pavani" << std::endl;
+    std::cout << "I am learning C++" << std::endl;
     return 0;
 }
