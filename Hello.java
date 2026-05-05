75478f403fe84938ebf8d5363516579f054a2188
Pavani Yadav <pavaniyadav093@gmail.com>
2026-05-05 14:48:58 +0000

    Add Hello.java - first Java program

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java on my HP Chromebook!");
+        System.out.println("My name is Pavani");
+        System.out.println("I just installed JDK 21");
+    }
+}
